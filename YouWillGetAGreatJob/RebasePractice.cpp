78438c00b57d99b666b3fe29eaee1ab9031fae15
Michal <mkalit@campus.technion.ac.il>
2021-03-10 10:48:48 +0200

    Add feature branch

--- YouWillGetAGreatJob/RebasePractice.cpp
+++ YouWillGetAGreatJob/RebasePractice.cpp
@@ -3,4 +3,11 @@ using namespace std;
 
 int main() {
 	cout << "This is the master branch" << endl;
+
+	cout << "Rebased file" << endl;
+}
+
+
+void Feature(){
+	cout << "This is the feature branch" << endl;
 }
